57c7c6cca73b3541e935b57e0c7fe98e71d62b77
ramana056 <ramanakommaraju200@gmail.com>
2023-05-12 12:17:16 +0530

    ll

--- src/main/java/test/firstone.java
+++ src/main/java/test/firstone.java
@@ -13,7 +13,7 @@ public class firstone {
      */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Hello");
+        System.out.println("Hello ");
 
     }
 
