f2f912992dc90cd7816ddbef8ee5d5afacd9da0c
ramana056 <ramanakommaraju200@gmail.com>
2023-05-12 12:15:48 +0530

    adding code

---
+++ src/main/java/test/firstone.java
@@ -0,0 +1,20 @@
+package test;
+
+/**
+ * TODO: Document me!
+ *
+ * @author koramana
+ *
+ */
+public class firstone {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello");
+
+    }
+
+}
